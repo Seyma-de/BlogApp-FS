726ca658ca3b894535ff6460940791c923c1216e
seyma <betul.cagil.krdmn@gmail.com>
2023-12-22 21:06:39 +0100

    backend  started

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:4000", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
   return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="text" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Register</button>
     </form>
   );
