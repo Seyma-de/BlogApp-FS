6bd57b62480ba32ccc9ddc80d86ed7ef54a24fb9
seyma <betul.cagil.krdmn@gmail.com>
2023-12-22 14:52:57 +0100

    login register

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,22 @@
 import "./App.css";
 import Header from "./Header";
+import Layout from "./Layout";
 import Post from "./Post";
+
 import { Route, Routes } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import IndexPage from "./pages/IndexPage";
+
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
-      <Route
-        path={"/login"}
-        element={
-          <main>
-            <Header />
-            <div>login page</div>
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/register"} element={<RegisterPage />} />
+      </Route>
     </Routes>
   );
 }
